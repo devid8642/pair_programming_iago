11d8209d1e4214031c9c119b27a19851028b45f4
devid <nnick4673@gmail.com>
2023-06-07 21:03:05 -0300

    questão 1

---
+++ q1/main.py
@@ -0,0 +1,22 @@
+from unicodedata import normalize
+
+if __name__ == '__main__':
+    inp = input('Entre com uma palavra ou frase: ').strip().upper().replace(' ', '')
+    inp_normalizado = normalize('NFKD', inp).encode('ASCII', 'ignore').decode('ASCII')
+    aux = []
+    pontuacoes = [
+        '!', '?', '.', ',', ':',
+        ';', '/', '(', ')', '-', '"',
+        "'"
+    ]
+    for pont in pontuacoes:
+        if pont in inp_normalizado:
+            inp_normalizado = inp_normalizado.replace(pont, '')
+    for i in range(len(inp_normalizado) - 1, -1, -1):
+        if inp_normalizado[i] not in pontuacoes:
+            aux.append(inp_normalizado[i])
+    if inp_normalizado == ''.join(aux):
+        print(f'{inp} é um palíndromo')
+    else:
+        print(f'{inp} não é um palíndromo')
+    print(inp_normalizado)
